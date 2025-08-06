7f16fef7a430f5df80a09e1430a841f7769caea1
Jorge Arias <94802496+joariasdev@users.noreply.github.com>
2025-08-06 19:51:59 -0400

    Add data context skeleton

--- GameStore.Persistence/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GameStore.Persistence
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ GameStore.Persistence/DataContext.cs
@@ -0,0 +1,10 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    }
+}
