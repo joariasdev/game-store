e9a4acc777bf5a50e04c1531d0021f9738c15740
Jorge Arias <94802496+joariasdev@users.noreply.github.com>
2025-08-09 19:18:24 -0400

    Change labels for customers views

--- GameStore.Domain/Customer.cs
+++ GameStore.Domain/Customer.cs
@@ -1,10 +1,16 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace GameStore.Domain
 {
     public class Customer
     {
         public int Id { get; set; }
+
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
