d5b1678e9e1987caf96bd3b2edb5bc048308bf20
Jorge Arias <94802496+joariasdev@users.noreply.github.com>
2025-08-14 18:30:56 -0400

    Add invoices feature

---
+++ GameStore.Application/Services/InvoiceService.cs
@@ -0,0 +1,80 @@
+﻿using GameStore.Domain.Entities;
+using GameStore.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Application.Services
+{
+    public class InvoicesService
+    {
+        private readonly DataContext _context;
+        public InvoicesService(DataContext context)
+        {
+            _context = context;
+        }
+
+        // Get all invoices
+        public async Task<List<Invoice>> GetAll()
+        {
+            return await _context.Invoices.ToListAsync();
+        }
+
+        // Get a invoice by id
+        public async Task<Invoice?> GetById(int? id)
+        {
+            var invoice = await _context.Invoices
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            return invoice;
+        }
+
+        // Create a new invoice in database
+        public async Task<bool> Create(Invoice invoice)
+        {
+            try
+            {
+                _context.Add(invoice);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+        // Update an existing invoice in database
+        public async Task<bool> Update(Invoice invoice)
+        {
+            try
+            {
+
+                _context.Update(invoice);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        // Remove one invoice from database
+        public async Task<bool> Remove(Invoice invoice)
+        {
+            try
+            {
+
+                _context.Remove(invoice);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
---
+++ GameStore.Web/Controllers/InvoicesController.cs
@@ -0,0 +1,142 @@
+﻿using GameStore.Application.Services;
+using GameStore.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameStore.Web.Controllers
+{
+    public class InvoicesController : Controller
+    {
+        private readonly InvoicesService _invoicesService;
+
+        public InvoicesController(InvoicesService invoicesService)
+        {
+            _invoicesService = invoicesService;
+        }
+
+        // GET: Invoices
+        public async Task<IActionResult> Index()
+        {
+            var invoices = await _invoicesService.GetAll();
+            return View(invoices);
+        }
+
+        // GET: Invoices/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var invoice = await _invoicesService.GetById(id);
+
+            if (invoice == null)
+            {
+                return NotFound();
+            }
+
+            return View(invoice);
+        }
+
+        // GET: Invoices/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Invoices/Create     
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Invoice invoice)
+        {
+            if (ModelState.IsValid)
+            {
+                var createdInvoice = await _invoicesService.Create(invoice);
+
+                if (createdInvoice)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(invoice);
+        }
+
+        // GET: Invoices/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var invoice = await _invoicesService.GetById(id);
+
+            if (invoice == null)
+            {
+                return NotFound();
+            }
+            return View(invoice);
+        }
+
+        // POST: Invoices/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Invoice invoice)
+        {
+            if (id != invoice.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var updatedInvoice = await _invoicesService.Update(invoice);
+
+                if (updatedInvoice)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(invoice);
+        }
+
+        // GET: Invoices/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var invoice = await _invoicesService.GetById(id);
+
+            if (invoice == null)
+            {
+                return NotFound();
+            }
+
+            return View(invoice);
+        }
+
+        // POST: Invoices/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var invoice = await _invoicesService.GetById(id);
+
+            if (invoice != null)
+            {
+
+                await _invoicesService.Remove(invoice);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool InvoiceExists(int id)
+        {
+            var invoice = _invoicesService.GetById(id);
+            return invoice != null;
+        }
+    }
+}
